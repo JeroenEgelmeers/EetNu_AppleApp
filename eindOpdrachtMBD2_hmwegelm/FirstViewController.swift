99369220a5be9d13ea997e78966ebf6c0edf3e51
User <user@Users-Mac.local>
2015-04-04 12:01:31 -0700

    Initial Commit

---
+++ eindOpdrachtMBD2_hmwegelm/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  eindOpdrachtMBD2_hmwegelm
+//
+//  Created by User on 04/04/15.
+//  Copyright (c) 2015 hmwegelm2060058. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ eindOpdrachtMBD2_hmwegelm/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  eindOpdrachtMBD2_hmwegelm
+//
+//  Created by User on 04/04/15.
+//  Copyright (c) 2015 hmwegelm2060058. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
